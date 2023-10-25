06cd4036a9b64cbd7af60afa0e0b040702279fcf
kalyangoparaju <kalyangoparaju31@gmail.com>
2023-10-26 02:15:47 +0530

    cors modified

--- server/server.js
+++ server/server.js
@@ -19,7 +19,11 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(cors({
+  origin:["https://blog-front-mauve.vercel.app"],
+  methods:['POST','GET'],
+  credentials:true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
